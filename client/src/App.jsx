2df5283d5b5569a1204e73c831f9045f19610b77
Ayush <85547808+AyushK-26@users.noreply.github.com>
2026-06-21 23:16:03 +0530

    Add Socket.IO integration and project setup files

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,16 @@
-import React from "react";
+import { io } from "socket.io-client";
+import { useEffect } from "react";
 
 const App = () => {
-  return <div>App</div>;
+  useEffect(() => {
+    const socket = io("http://localhost:3000");
+    socket.on("connect", () => console.log("connected:", socket.id));
+
+    return () => {
+      socket.disconnect();
+    };
+  }, []);
+  return <div>Signaling test</div>;
 };
 
 export default App;
--- server/index.js
+++ server/index.js
@@ -1,11 +1,19 @@
 const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+
 const app = express();
+const server = http.createServer(app);
+const io = new Server(server, { cors: { origin: "*" } });
+
 const port = 3000;
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-app.listen(port, () => {
+io.on("connection", (socket) => console.log("connected: ", socket.id));
+
+server.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
